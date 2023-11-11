07c9998a4c71b7151b769324cf95e5fd2c379044
wifitanmalo <nicolas.chaparro@correounivalle.edu.co>
2023-11-10 22:00:07 -0500

    Dictionaries were implemented in all functions

--- the_crud.py
+++ the_crud.py
@@ -10,8 +10,9 @@ def choice_selection():
             print("----- Only can enter C, R, U, D or E -----")
     return choice
 
+
 def create():
-    global name, users_list, number
+    global users_list, number
     while True:
         name = input("- Enter the name to create: ").capitalize()
         if name.isalpha():
@@ -21,18 +22,19 @@ def create():
     if name in users_list:
         print(f"----- '{name}' was created again -----")
     else:
-        print(f"----- '{name}' was created successfully -----")
+        print(f"----- '{name}' was created -----")
     number += 1
-    users_list |= {f"Name {number}":name}
+    users_list |= {number:name}
 
 
 def read():
     search = input("- Enter the name to search: ").capitalize()   
-    for position in range(1, number+1):
-        if search not in users_list.values():
-            return "----- Sorry, the name isn't in our database -----"
-        elif search == users_list[f"Name {position}"]:
-            return "  + Hi, {}!".format(users_list[f"Name {position}"])
+    if search not in users_list.values():
+        return(f"----- '{search}' isn't in our database -----")
+    else:
+        for name_number in range(1, number+1):
+            if users_list[name_number] == search:
+                return(f"  + Hi, {search}! {users_list}")
 
 
 # ----- List of names -----""")
@@ -42,27 +44,33 @@ def read():
 
 def update():
     update = input("- Enter the name to update: ").capitalize()
-    if update in users_list:
+    if update in users_list.values():
         while True:
             new = input("  + Enter the new name: ").capitalize()
             if new.isalpha():
                 break
             else:
                 print("  ----- The name only can contain WORDS -----")
-        position = users_list.index(update)
-        users_list[position] = new
-        print(f"----- '{update}' was updated to '{new}' successfully -----")
+        for name_number in range(1, number+1):
+            if users_list[name_number] == update:
+                users_list[name_number] = new
+                break
+        return(f"----- '{update}' was updated to '{new}' -----")
     else:
-        print("----- Sorry, the name isn't in our database -----")
+        return(f"----- '{update}' isn't in our database -----")
 
 
 def delete():
+    global number
     delete = input("- Enter the name to delete: ").capitalize()
-    if delete in users_list:
-        users_list.remove(delete)
-        print(f"----- '{delete}' was deleted successfully -----")
+    if delete in users_list.values():
+        for name_number in range(1, number+1):
+            if users_list[name_number] == delete:
+                number -= 1
+                users_list.pop(name_number)
+        return(f"----- '{delete}' was deleted -----")
     else:
-        print("----- Sorry, the name isn't in our database -----")
+        return(f"----- '{delete}' isn't in our database -----")
 
 
 def exit():
@@ -92,9 +100,9 @@ Welcome! What would you to like today?
             elif choice == "R" and len(users_list) > 0:
                 print(read())
             elif choice == "U" and len(users_list) > 0:
-                update()
+                print(update())
             elif choice == "D" and len(users_list) > 0:
-                delete()
+                print(delete())
             elif choice == "E":
                 print(exit())
                 break
